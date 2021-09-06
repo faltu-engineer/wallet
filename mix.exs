5dcfa9ee45ffd38ef322a1a30c253e76cecf330e
Rahul Rajput <rahul@dbadbadba.com>
2021-09-06 23:24:03 +0530

    t

--- mix.exs
+++ mix.exs
@@ -28,6 +28,8 @@ defmodule Wallet.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp elixirc_options(_), do: [warnings_as_errors: false]
+
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
