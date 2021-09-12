7342291be2159c0ab206feedb67f95f8af51548d
Rahul Rajput <rahul@dbadbadba.com>
2021-09-12 19:08:40 +0530

    first working mockup

--- lib/wallet_web/controllers/page_controller.ex
+++ lib/wallet_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule WalletWeb.PageController do
   use WalletWeb, :controller
 
+  alias Wallet.Expenses
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    expenses = Expenses.list_expenses()
+    render(conn, "index.html", expenses: expenses)
   end
 end
